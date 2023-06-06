c79bfee4f4f307d3a809135152198f141426ebf1
刘平平 <liupingping@purvar.com>
2023-06-06 14:57:09 +0800

    refactor:增加CSDN文章的处理

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,5 @@
 console.log("start reading time");
-const article = document.getElementById("main-content");
+const article = document.querySelector("main");
 
 // `document.querySelector` may return null if the selector doesn't match anything.
 if (article) {
@@ -9,6 +9,7 @@ if (article) {
   const words = text.matchAll(wordMatchRegExp);
   // matchAll returns an iterator, convert to array to get word count
   const wordCount = [...words].length;
+  // assume that people read 200 words every minute
   const readingTime = Math.round(wordCount / 200);
   const badge = document.createElement("p");
   // Use the same styling as the publish information in an article's header
@@ -16,10 +17,11 @@ if (article) {
   badge.textContent = `⏱️ ${readingTime} min read -- 熊猫Jay`;
 
   // Support for API reference docs
-  const heading = article.querySelector("h1");
+  const heading = article.querySelector(".title-article");
   // Support for article docs with date
   const date = article.querySelector("time")?.parentNode;
-
+  console.log("heading", heading);
+  console.log("date", date);
   (date ?? heading).insertAdjacentElement("afterend", badge);
 } else {
   console.log("this page does not contain article");
