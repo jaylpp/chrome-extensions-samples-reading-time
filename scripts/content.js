d413155b6baa2bec65743f0362d35921cc6c20e7
刘平平 <liupingping@purvar.com>
2023-06-06 14:13:18 +0800

    feat:增加 Chrome DevTools 和 Workbox相关页面的兼容

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,9 @@
-console.log("start");
-const article = document.querySelector("article");
+console.log("start reading time");
+const article = document.getElementById("main-content");
 
 // `document.querySelector` may return null if the selector doesn't match anything.
 if (article) {
+  console.log("this page contains article");
   const text = article.textContent;
   const wordMatchRegExp = /[^\s]+/g; // Regular expression
   const words = text.matchAll(wordMatchRegExp);
@@ -12,7 +13,7 @@ if (article) {
   const badge = document.createElement("p");
   // Use the same styling as the publish information in an article's header
   badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read -- Jay`;
+  badge.textContent = `⏱️ ${readingTime} min read -- 熊猫Jay`;
 
   // Support for API reference docs
   const heading = article.querySelector("h1");
@@ -20,4 +21,7 @@ if (article) {
   const date = article.querySelector("time")?.parentNode;
 
   (date ?? heading).insertAdjacentElement("afterend", badge);
+} else {
+  console.log("this page does not contain article");
 }
+console.log("end reading time");
